9f892b3a3f4b31a5b6791cc3d559e4dd90cfec69
Thomas Hayes <hayes.thomas610@gmail.com>
2022-11-14 10:55:06 -0700

    test built out and functionality complete

--- spec/requests/tourist_sights_request_spec.rb
+++ spec/requests/tourist_sights_request_spec.rb
@@ -9,6 +9,14 @@ RSpec.describe 'tourist sights API request from FE' do
 
       expect(response).to be_successful
       expect(tourist_sights).to be_a Hash
+
+      tourist_sights[:data].each do |sight|
+        expect(sight.keys). to eq([:id, :type, :attributes])
+        expect(sight[:attributes].keys).to eq([:name, :address, :place_id])
+        expect(sight[:id]).to eq(nil)
+        expect(sight[:type]).to eq("tourist_sight")
+        expect(sight[:attributes]).to be_a Hash
+      end
     end
   end
 end
