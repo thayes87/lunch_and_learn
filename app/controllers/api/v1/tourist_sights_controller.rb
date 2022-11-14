3f3b568c49491696a50a279d5d81f5c08dabb5e2
Thomas Hayes <hayes.thomas610@gmail.com>
2022-11-14 11:49:23 -0700

    extension 1 - add logic to tourist sights controller to add random country if not passed in

--- app/controllers/api/v1/tourist_sights_controller.rb
+++ app/controllers/api/v1/tourist_sights_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TouristSightsController < ApplicationController
   def index
-    country = params[:format]
+    country = find_country
     lat, long = find_latlong(country)
     tourist_sights = TouristSightsService.get_tourist_sights(lat, long)
     render json: TouristSightsSerializer.results(tourist_sights)
@@ -10,5 +10,13 @@ class Api::V1::TouristSightsController < ApplicationController
 
   def find_latlong(country)
     CountryService.get_latlong(country)
+  end
+
+  def find_country
+    if params[:format].nil?
+      CountryService.get_random_country
+    else
+      params[:format]
+    end
   end
 end
--- spec/requests/tourist_sights_request_spec.rb
+++ spec/requests/tourist_sights_request_spec.rb
@@ -1,12 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe 'tourist sights API request from FE' do
+  #happy path
   context 'when given a valid country' do 
     it 'returns tourists sights for the given country within a 20000 meter radius of the capital' do
       get api_v1_tourist_sights_path("france")
 
       tourist_sights = JSON.parse(response.body, symbolize_names: true)
 
+      expect(response).to be_successful
+      expect(tourist_sights).to be_a Hash
+
+      tourist_sights[:data].each do |sight|
+        expect(sight.keys). to eq([:id, :type, :attributes])
+        expect(sight[:attributes].keys).to eq([:name, :address, :place_id])
+        expect(sight[:id]).to eq(nil)
+        expect(sight[:type]).to eq("tourist_sight")
+        expect(sight[:attributes]).to be_a Hash
+      end
+    end
+  end
+  #sad path
+  context 'when no country is given' do
+    it 'returns recipes for a random country' do
+      allow(CountryService).to receive(:get_random_country).and_return("Germany")
+      get api_v1_tourist_sights_path()
+
+      tourist_sights = JSON.parse(response.body, symbolize_names: true)
+
+      expect(CountryService).to have_received(:get_random_country)
       expect(response).to be_successful
       expect(tourist_sights).to be_a Hash
 
