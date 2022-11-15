dd82c92bd8f08325d232a7243a59d4f5ac765bb8
Thomas Hayes <hayes.thomas610@gmail.com>
2022-11-15 13:24:03 -0700

    finalize happy path for User Registration

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,16 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :id, :name, :email, :api_key
+  def self.new(user)
+    {
+      "data": {
+        "type": "user",
+        "id": user.id,
+        "attributes": {
+          "name": user.name,
+          "email": user.email,
+          "api_key": user.api_key
+        }
+      }
+    }
+  end
 end
--- spec/requests/user_registration_request_spec.rb
+++ spec/requests/user_registration_request_spec.rb
@@ -12,11 +12,17 @@ RSpec.describe 'User request APIs' do
       
       post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
 
-      created_user = User.last
+      new_user = User.last
+      new_user = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_successful
-      expect(created_user.name).to eq(user_params[:name])
-      expect(created_user.email).to eq(user_params[:email])
+      expect(new_user).to be_a Hash
+
+      expect(new_user[:data].keys).to eq([:type, :id, :attributes])
+      expect(new_user[:data][:attributes].keys).to eq([:name, :email, :api_key])
+      expect(new_user[:data][:id]).to be_a Integer
+      expect(new_user[:data][:type]).to eq("user")
+      expect(new_user[:data][:attributes]).to be_a Hash
     end
   end
 end
